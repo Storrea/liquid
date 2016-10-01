ff77fe29e7e5721210383be4ee58ea23a0cb3d4f
S M Shahnewaz <shawniut@gmail.com>
2016-10-01 03:45:27 -0600

    Fixed encoding bug

--- lib/liquid/block_body.rb
+++ lib/liquid/block_body.rb
@@ -107,7 +107,7 @@ module Liquid
 
     def render_node(node, context)
       node_output = (node.respond_to?(:render) ? node.render(context) : node)
-      node_output = node_output.is_a?(Array) ? node_output.map{|e| e.to_s.force_encoding('UTF-8')}.join : node_output.to_s.force_encoding('UTF-8')
+      node_output = node_output.is_a?(Array) ? node_output.join : node_output.to_s
 
       context.resource_limits.render_length += node_output.length
       if context.resource_limits.reached?
